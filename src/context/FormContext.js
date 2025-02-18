cb003d2fd48dc452852e5a5980f3d8d0f51d1f5c
ankitkumardyotis <ankitkumar@dyotis.com>
2025-02-18 09:14:47 +0530

    change currentStep

--- src/context/FormContext.js
+++ src/context/FormContext.js
@@ -39,7 +39,7 @@ export function FormProvider({ children }) {
     emergencyFundAmount: "",
     emergencyFundMonths: "",
   });
-  const [currentStep, setCurrentStep] = useState(17);
+  const [currentStep, setCurrentStep] = useState(1);
   const [errors, setErrors] = useState({});
   const [sidebarOpen, setSidebarOpen] = useState(false);
 

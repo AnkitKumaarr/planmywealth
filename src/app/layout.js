30080d177b938b6f9de2373f8113545227a49513
ankitkumardyotis <ankitkumar@dyotis.com>
2025-03-09 18:16:53 +0530

    added sendmail.io link in the header

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { AppProvider } from "@/context/AppContext";
 import "./globals.css";
 import { FormProvider } from "../context/FormContext";
 import { AuthProvider } from "@/context/AuthContext";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,6 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.variable}`}>
+      <head>
+        <Script
+          id="ACXConnectScript"
+          src="https://app.sendmails.io/websites/67cd8d1a0c29b/connect.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body>
         <ThemeProvider>
           <AppProvider>
